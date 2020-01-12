c01e48f491d65033903beffb1e49354897f834b7
spb-superstruct <andrei@superstruct.tech>
2020-01-11 22:51:09 -0800

    feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
   if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
   function respondBase64 (req, res) {
     const phrase = req.url.replace(/^\/b64\//, '')
@@ -15,6 +16,11 @@ const server = http.createServer((req, res) => {
 
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
+}
+
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+  res.end(JSON.stringify({ ua }))
 }
 
 server.listen(PORT)
