278e9fdb96f2d7da848ff759f87e457628f50355
spb-superstruct <andrei@superstruct.tech>
2020-01-11 22:45:48 -0800

    feat: add base64 endpoint

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
+  function respondBase64 (req, res) {
+    const phrase = req.url.replace(/^\/b64\//, '')
+    res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
+  }
 
   res.end()
 })
